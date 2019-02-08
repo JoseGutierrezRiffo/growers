f853456fa3d4ed66a71dcff433bf7ff68264fc7b
Alejandro <icce.redes@gmail.com>
2019-02-08 18:05:53 -0300

    Start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBmX802Z21LkiOTx83JZBcDQDLxRT5_GZw",
+    authDomain: "growers-eaf3f.firebaseapp.com",
+    databaseURL: "https://growers-eaf3f.firebaseio.com",
+    projectId: "growers-eaf3f",
+    storageBucket: "growers-eaf3f.appspot.com",
+    messagingSenderId: "12847442961"
+});
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
